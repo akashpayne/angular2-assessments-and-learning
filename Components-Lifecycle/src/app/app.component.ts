8411ee4a10f1e3f1a2bbe15b402de360aa622d43
Akash Payne <akashpayne@gmail.com>
2017-08-04 10:48:11 +0100

    Added project files to git
    Angular 2 project files

---
+++ Components-Lifecycle/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import { LifecycleComponent } from './lifecycle.component';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  template: `
+    <h1>Root Component</h1>
+    <fa-lifecycle *ngIf="!delete" [bindable]="boundValue">
+        <p #boundContent>{{test}}</p>
+    </fa-lifecycle>
+    <button (click)="delete = true">Click to Delete</button>
+    <button (click)="test = 'Changed value'">Click to Change Content</button>
+    <button (click)="boundValue = 2000">Click to Change Binding</button>
+  `
+})
+export class AppComponent {
+    delete = false;
+    test = 'Starting value';
+    boundValue = 1000;
+}
